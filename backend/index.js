baa010d22202552db89098f2975fbf83c98c2b1a
Sujeet Singh <sujeetsingh9933@gmail.com>
2025-10-07 15:55:38 +0530

    frontend url changed

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://two-airbnb-1.onrender.com",
     credentials:true
 }))
 
